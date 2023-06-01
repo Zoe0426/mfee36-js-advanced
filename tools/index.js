751bdaf1424b80925096ac99742de38dca5dbcf7
GB <zoe820426@gmail.com>
2023-06-01 18:01:26 +0800

    sort-btn+連結

---
+++ tools/index.js
@@ -0,0 +1,11 @@
+import Person,{data}from "./person.js";
+import a,{b,c}from "./my-js02.js";
+
+export{Person,data,a,b,c};
+
+
+/*
+
+export { default as Person, data } from "./person.js";
+export { default as a, b, c } from "./my-js02.js";
+*/
---
+++ tools/my-js02.js
@@ -0,0 +1,14 @@
+
+console.log('hello-js2');
+
+export default function a(n){
+    return n*n;
+}
+
+
+export const b=(n)=>{
+    return n*n*n;
+};
+
+const c =125;
+export {c};
